ac04eeb9d8b2d3fcab4af35c99da3db2559990aa
AtaMark <markjohnkeith2013@gmail.com>
2018-07-06 15:29:20 +0300

    Add project files.

---
+++ Impairement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Impairement.Startup))]
+namespace Impairement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
